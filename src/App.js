17f77aca61a8d76da10d5ced9ceb528d863851b1
Kevin K <kahwaikagwima@gmail.com>
2023-06-19 12:30:08 +0300

    Added React App components and JSX

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () =>{
+    const names = ["Bob","Kevin","Dave","Kahwai"];
+    const interger = Math.floor(Math.random()*3 + 1);
+    return names[interger];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {/*  Edit <code>src/App.js</code> edit and save to see changes */}
         </p>
-        <a
+        <p>Lets create a template html</p>
+        <p>Hello {handleNameChange()}!</p>
+
+        {/*   <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+
+        <p>{/* JSX accepts javascript expressions placed inside curly brackets as shown below, a string-{'kevin'}-,a number-{1}-,an array-{[1,2,3]}-.Even if you remove the curly braces JSX will render the data as text -[1,2,3]-.We cannot however display an object as shown or a boolean.Note if you want to comment something out then you have to do that inside of curly brackets */}</p>
+        <p>{/* {nameA} */}</p>
+        <p>{/* commenting done inside curly brackets */} {/* {nameA} */}</p>
       </header>
+      
     </div>
   );
 }
